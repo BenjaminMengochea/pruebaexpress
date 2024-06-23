25f3414944e597504c5d4814800809230a7f159c
BenjaminMengochea <127338344+BenjaminMengochea@users.noreply.github.com>
2024-06-23 16:36:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require ("morgan");
 const database = require( "./database");
+const cors = require ("cors")
 
 //Configuracion Inicial
 const app = express();
@@ -9,6 +10,9 @@ app.listen(app.get("port"));
 console.log("escuchando comunicaciones al puerto"+app.get("port"));
 
 //MiddleWares
+app.use(cors({
+    origin: ["http://127.0.0.1:5501","http://127.0.0.1:5500"]
+}))
 app.use(morgan("dev"));
 
 //Rutas
@@ -16,4 +20,10 @@ app.get("/productos", async (req,res)=>{
     const connection = await database.getconnection()
     const result = await connection.query("SELECT * from producto");
     res.json(result)
+})
+app.post("/carrito/comprar", async (req, res)=> {
+    if(req.body && req.body.length > 0){
+        return res.sendStatus(200);
+    }
+    res.sendStatus(400)
 })
